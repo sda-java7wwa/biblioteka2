b904bdc76ff61dbf5b16c57e131f083c0fd96c12
MarcinSwa <marcin600rr@hotmail.com>
2018-03-02 19:15:56 +0100

    struktura pakietow stan 0

--- src/library2/model/User.java
+++ src/library2/model/User.java
@@ -1,4 +1,10 @@
 package library2.model;
 
+import java.util.List;
+
 public class User {
+    private String name;
+    private String surname;
+    private String passwd;
+    private List<Book> booklist;
 }
