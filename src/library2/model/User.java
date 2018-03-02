987a78b56cc0b50cbfcf0fa52a8e394bc3ff56da
Łukasz Orzechowski <lu.k3x2@gmail.com>
2018-03-02 21:24:32 +0100

    user login

--- src/library2/model/User.java
+++ src/library2/model/User.java
@@ -8,12 +8,23 @@ public class User {
     private String surname;
     private String passwd;
     private List<Book> booklist;
+    private String login;
 
-    public User(String name, String surname, String passwd, List<Book> booklist) {
+    public User(String name, String surname, String passwd, List<Book> booklist, String login) {
         this.name = name;
         this.surname = surname;
         this.passwd = passwd;
         this.booklist = booklist;
+        this.login = login;
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
     }
 
     @Override
