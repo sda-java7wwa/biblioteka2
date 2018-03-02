4388fb49fb27c950a0aff537d71b7d9e99cb7a03
MarcinSwa <marcin600rr@hotmail.com>
2018-03-02 19:26:01 +0100

    struktura pakietow stan 0

--- src/library2/dao/UserDao.java
+++ src/library2/dao/UserDao.java
@@ -1,4 +1,10 @@
 package library2.dao;
+import library2.model.User;
+import java.util.List;
 
 public interface UserDao {
+
+    List<User> getUsers ();
+    boolean saveUser(List<User> users);
+
 }
