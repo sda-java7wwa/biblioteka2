e17715357ab3a155a2a73ef17ae360d023c81809
Bartek Polak <bpolak28@gmail.com>
2018-03-02 20:47:04 +0100

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,24 @@
 package com.company;
 
+import library2.dao.BooksDaoSerialImpl;
+import library2.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Marcin");
+        List<Book> bookList = new ArrayList<>();
 
-        System.out.println("Lukasz");
+        bookList.add(new Book("Ja","On",1999,1));
+        bookList.add(new Book("Ja2","On2",1998,2));
+        bookList.add(new Book("Ja3","On3",1997,3));
+        bookList.add(new Book("Ja4","On4",1996,4));
 
-        System.out.println("Bartek");
+        BooksDaoSerialImpl booksDaoSerial = new BooksDaoSerialImpl();
+
+        booksDaoSerial.saveBook(bookList);
     }
 }
--- src/library2/dao/BooksDaoSerialImpl.java
+++ src/library2/dao/BooksDaoSerialImpl.java
@@ -1,4 +1,35 @@
 package library2.dao;
 
-public class BooksDaoSerialImpl {
+import library2.model.Book;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+public class BooksDaoSerialImpl implements BookDao {
+
+
+
+    @Override
+    public List<Book> getBooks() {
+        return null;
+    }
+
+    @Override
+    public boolean saveBook(List<Book> books) {
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream("books.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(books);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized data is saved in books.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
