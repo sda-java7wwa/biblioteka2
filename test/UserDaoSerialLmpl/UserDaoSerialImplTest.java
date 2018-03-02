28873864e319ccd7721ef2cb14a68f2218005aaa
Łukasz Orzechowski <lu.k3x2@gmail.com>
2018-03-02 21:03:48 +0100

    test user

--- test/UserDaoSerialLmpl/UserDaoSerialImplTest.java
+++ test/UserDaoSerialLmpl/UserDaoSerialImplTest.java
@@ -1,11 +1,25 @@
 package UserDaoSerialLmpl;
 
+import library2.dao.UserDaoSerialImpl;
+import library2.model.User;
 import org.junit.Test;
+
+import javax.jws.soap.SOAPBinding;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDaoSerialImplTest {
 
+public UserDaoSerialImpl user = new UserDaoSerialImpl();
+
 @Test
     public void saveUserTest () {
+
+
+    User user1 = new User("Jnusz","Biznesy","hasło",null);
+    List<User> list = new ArrayList<>();
+
+    user.saveUser(list);
 
 }
 }
