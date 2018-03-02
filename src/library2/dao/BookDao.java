402160839d187b85b98080ee8076b4f4e4330e4d
Bartek Polak <bpolak28@gmail.com>
2018-03-02 20:19:12 +0100

    initial commit

--- src/library2/dao/BookDao.java
+++ src/library2/dao/BookDao.java
@@ -1,4 +1,12 @@
 package library2.dao;
 
+import library2.model.Book;
+
+import java.util.List;
+
 public interface BookDao {
+
+    List<Book> getBooks();
+
+    boolean saveBook(List<Book> books);
 }
