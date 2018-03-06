3608ab330ca25fc82b4d3caa8e67e26bcae7d5c4
Bartek Polak <bpolak28@gmail.com>
2018-03-06 21:21:01 +0100

    userDao poprawka

--- test/UserDaoSerialLmpl/BooksDaoSerialImplTest.java
+++ test/UserDaoSerialLmpl/BooksDaoSerialImplTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class BooksDaoSerialImplTest {
 
-    public BooksDaoSerialImpl booksDaoSerial = new BooksDaoSerialImpl();
+    public BooksDaoSerialImpl booksDaoSerial = new BooksDaoSerialImpl("booksTest.ser");
 
     @Test
     public void BooksDaoTest () {
