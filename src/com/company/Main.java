1f402861eb682aafb0dc1a7be3eb514d15eece99
Bartek Polak <bpolak28@gmail.com>
2018-03-07 19:09:20 +0100

    userDao poprawka

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import library2.dao.BooksDaoSerialImpl;
 import library2.dao.UserDao;
 import library2.dao.UserDaoSerialImpl;
 import library2.model.Book;
+import library2.model.Category;
 import library2.model.User;
 
 import java.util.ArrayList;
@@ -12,30 +13,81 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        List<Book> bookList = new ArrayList<>();
 
-        bookList.add(new Book("Ja","On",1999,1));
-        bookList.add(new Book("Ja2","On2",1998,2));
-        bookList.add(new Book("Ja3","On3",1997,3));
-        bookList.add(new Book("Ja4","On4",1996,4));
+        Category main = new Category("Główna");
 
-////        BooksDaoSerialImpl booksDaoSerial = new BooksDaoSerialImpl();
+        Category poradniki = new Category("Poradniki");
+
+        main.addToList(poradniki);
+
+        Category biznesowe = new Category("Biznesowe");
+
+        poradniki.addToList(biznesowe);
+
+        biznesowe.addToList(new Book("Sienkiewicz","Ogniem i Mieczem",1920,1));
+        biznesowe.addToList(new Book("Sienkiewicz1","Ogniem i Mieczem",1920,2));
+        biznesowe.addToList(new Book("Sienkiewicz2","Ogniem i Mieczem",1920,3));
+
+        poradniki.addToList(new Book("poradnik1","tytul",1999,4));
+        poradniki.addToList(new Book("poradnik2","tytul",1999,5));
+
+        Category kulinarne = new Category("kulinarne");
+
+        poradniki.addToList(kulinarne);
+
+        kulinarne.addToList(new Book("Okrasa","Pomidorowa",2019,6));
+        kulinarne.addToList(new Book("Okrasa2","Pomidorowa",2019,7));
+        kulinarne.addToList(new Book("Paskal","Rosół",2019,8));
+
+        System.out.println(main);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//	// write your code here
+//        List<Book> bookList = new ArrayList<>();
 //
-//        booksDaoSerial.saveBook(bookList);
+//        bookList.add(new Book("Ja","On",1999,1));
+//        bookList.add(new Book("Ja2","On2",1998,2));
+//        bookList.add(new Book("Ja3","On3",1997,3));
+//        bookList.add(new Book("Ja4","On4",1996,4));
 //
-//        System.out.println(booksDaoSerial.getBooks());
-
-//        List<User> userList = new ArrayList<>();
-//        userList.add(new User("bartek","polak","aaa",null,"bbb"));
-//        userList.add(new User("bartek1","polak1","aaa1",null,"bbb1"));
-//        userList.add(new User("bartek2","polak2","aaa2",null,"bbb2"));
-//        userList.add(new User("bartek3","polak3","aaa3",null,"bbb3"));
-
-        UserDao userDao = new UserDaoSerialImpl();
-
- //       userDao.saveUser(userList);
-
-        System.out.println(userDao.getUsersData());
+//////        BooksDaoSerialImpl booksDaoSerial = new BooksDaoSerialImpl();
+////
+////        booksDaoSerial.saveBook(bookList);
+////
+////        System.out.println(booksDaoSerial.getBooks());
+//
+////        List<User> userList = new ArrayList<>();
+////        userList.add(new User("bartek","polak","aaa",null,"bbb"));
+////        userList.add(new User("bartek1","polak1","aaa1",null,"bbb1"));
+////        userList.add(new User("bartek2","polak2","aaa2",null,"bbb2"));
+////        userList.add(new User("bartek3","polak3","aaa3",null,"bbb3"));
+//
+//        UserDao userDao = new UserDaoSerialImpl("users.ser");
+//
+// //       userDao.saveUser(userList);
+//
+//        System.out.println(userDao.getUsersData());
     }
 }
---
+++ src/library2/model/Category.java
@@ -0,0 +1,53 @@
+package library2.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category {
+    private String name;
+    private List<Category> subcategory;
+    private List<Book> bookList;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getNazwa() {
+        return name;
+    }
+
+    public void addToList(Book book){
+        if(bookList==null){
+            bookList=new ArrayList<>();
+        }
+        bookList.add(book);
+    }
+
+    public void setNazwa(String nazwa) {
+        this.name = nazwa;
+    }
+
+    public List<Category> getSubcategory() {
+        return subcategory;
+    }
+
+    public void addToList(Category category) {
+        if(subcategory==null){
+            subcategory=new ArrayList<>();
+        }
+        subcategory.add(category);
+    }
+
+    public void setSubcategory(List<Category> podkategorie) {
+        this.subcategory = podkategorie;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                ", subcategory=" + getSubcategory() +
+                ", bookList=" + bookList +
+                '}';
+    }
+}
