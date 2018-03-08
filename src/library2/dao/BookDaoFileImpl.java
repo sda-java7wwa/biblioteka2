179f813b51278bf2e63bd2dfac0f0eab98500b8b
MarcinSwar <C1nx@github>
2018-03-08 18:51:55 +0100

    Finite State Machine _ mm

---
+++ src/library2/dao/BookDaoFileImpl.java
@@ -0,0 +1,15 @@
+package library2.dao;
+
+import library2.model.Category;
+
+public class BookDaoFileImpl implements BookDao {
+    @Override
+    public Category getBooks() {
+        return null;
+    }
+
+    @Override
+    public boolean saveBook(Category main) {
+        return false;
+    }
+}
--- src/library2/service/BookServiceImpl.java
+++ src/library2/service/BookServiceImpl.java
@@ -33,4 +33,6 @@ public class BookServiceImpl implements BookService {
 
         return booksDaoSerial.getBooks();
     }
+
+
 }
