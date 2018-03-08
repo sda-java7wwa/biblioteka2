01a2ef57d7fd1b03dde900b87320703080a07369
Łukasz Orzechowski <lu.k3x2@gmail.com>
2018-03-08 19:53:46 +0100

    wyśwuetlanie książek z kategorii definiowanych przez usera

--- src/library2/service/CategoryService.java
+++ src/library2/service/CategoryService.java
@@ -12,19 +12,24 @@ public class CategoryService {
     public void getBokList (Category category){    // prints List <Book> by category
 
         Scanner sc = new Scanner(System.in);
-        int myCase;
+        int myCase = 0;
+        int save;
         List<Category> cate =  category.getSubcategory();
-        List<Book> books = category.getBookList();
+
         System.out.println("Przejdź do wybranej kategorii , 0 wyświetla listę książek");
         while (cate != null) {
             for (int i = 0; i < cate.size(); i++) {
                 System.out.println(i + 1 + ". " + cate.get(i).getNazwa());
             }
+            save = myCase;
             myCase = sc.nextInt();
-            if (myCase == 0) {
-                System.out.println(category.getBookList());
+            if (myCase == 0 || cate.get(save).getSubcategory() == null) {
+                System.out.println(cate.get(save).getBookList());
             } else {
-                cate = cate.get(myCase).getSubcategory();
+                cate = cate.get(myCase - 1).getSubcategory();
+                if (cate == null){
+                    System.out.println(cate.get(save).getBookList());
+                }
             }
         }
 
